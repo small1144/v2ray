22ddfa96837760ee3609d019d915f4321ddb0dff
small1144 <155062396+small1144@users.noreply.github.com>
2024-06-29 20:58:32 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,283 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var client = &http.Client{}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func main() {
+
+	configFile := "channels.txt"
+	channels, err := readLines(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	configs := map[string]string{
+		"ss":     "",
+		"vmess":  "",
+		"trojan": "",
+		"vless":  "",
+		"mixed":  "",
+	}
+
+	myregex := map[string]string{
+		"ss":     `(.{3})ss:\/\/`,
+		"vmess":  `vmess:\/\/`,
+		"trojan": `trojan:\/\/`,
+		"vless":  `vless:\/\/`,
+	}
+
+	//protocol := ""
+	for i := 0; i < len(channels); i++ {
+		all_messages := false
+		if strings.Contains(channels[i], "{all_messages}") {
+			all_messages = true
+			channels[i] = strings.Split(channels[i], "{all_messages}")[0]
+		}
+
+		req, err := http.NewRequest("GET", channels[i], nil)
+		if err != nil {
+			log.Fatalf("Error When requesting to: %d Error : %s", channels[i], err)
+		}
+
+		resp, err1 := client.Do(req)
+		if err1 != nil {
+			log.Fatal(err1)
+		}
+		defer resp.Body.Close()
+
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		messages := doc.Find(".tgme_widget_message_wrap").Length()
+		link, exist := doc.Find(".tgme_widget_message_wrap .js-widget_message").Last().Attr("data-post")
+		if messages < 100 && exist == true {
+			number := strings.Split(link, "/")[1]
+			fmt.Println(number)
+
+			doc = GetMessages(100, doc, number, channels[i])
+		}
+
+		if all_messages {
+			fmt.Println(doc.Find(".js-widget_message_wrap").Length())
+			doc.Find(".tgme_widget_message_text").Each(func(j int, s *goquery.Selection) {
+				// For each item found, get the band and title
+				message_text := s.Text()
+				lines := strings.Split(message_text, "\n")
+				for a := 0; a < len(lines); a++ {
+					for _, regex_value := range myregex {
+						re := regexp.MustCompile(regex_value)
+						lines[a] = re.ReplaceAllStringFunc(lines[a], func(match string) string {
+							return "\n" + match
+						})
+					}
+					for proto, _ := range configs {
+						if strings.Contains(lines[a], proto) {
+							configs["mixed"] += "\n" + lines[a] + "\n"
+						}
+					}
+				}
+
+			})
+		} else {
+			doc.Find("code,pre").Each(func(j int, s *goquery.Selection) {
+				// For each item found, get the band and title
+				message_text := s.Text()
+				lines := strings.Split(message_text, "\n")
+				for a := 0; a < len(lines); a++ {
+					for proto_regex, regex_value := range myregex {
+						re := regexp.MustCompile(regex_value)
+						lines[a] = re.ReplaceAllStringFunc(lines[a], func(match string) string {
+							if proto_regex == "ss" {
+								if match[:3] == "vme" {
+									return "\n" + match
+								} else if match[:3] == "vle" {
+									return "\n" + match
+								} else {
+									return "\n" + match
+								}
+							} else {
+								return "\n" + match
+							}
+						})
+
+						if len(strings.Split(lines[a], "\n")) > 1 {
+							myconfigs := strings.Split(lines[a], "\n")
+							for i := 0; i < len(myconfigs); i++ {
+								if myconfigs[i] != "" {
+									re := regexp.MustCompile(regex_value)
+									myconfigs[i] = strings.ReplaceAll(myconfigs[i], " ", "")
+									match := re.FindStringSubmatch(myconfigs[i])
+									if len(match) >= 1 {
+										if proto_regex == "ss" {
+											if match[1][:3] == "vme" {
+												configs["vmess"] += "\n" + myconfigs[i] + "\n"
+											} else if match[1][:3] == "vle" {
+												configs["vless"] += "\n" + myconfigs[i] + "\n"
+											} else {
+												configs["ss"] += "\n" + myconfigs[i][3:] + "\n"
+											}
+										} else {
+											configs[proto_regex] += "\n" + myconfigs[i] + "\n"
+										}
+									}
+
+								}
+
+							}
+						}
+					}
+				}
+			})
+		}
+
+	}
+
+	for proto, configcontent := range configs {
+		// 		reverse mode :
+		// 		lines := strings.Split(configcontent, "\n")
+		// 		reversed := reverse(lines)
+		// 		WriteToFile(strings.Join(reversed, "\n"), proto+"_kuyshare.txt")
+		// 		simple mode :
+		WriteToFile(RemoveDuplicate(configcontent), proto+"_kuyshare.txt")
+	}
+
+}
+
+func WriteToFile(fileContent string, filePath string) {
+
+	// Check if the file exists
+	if _, err := os.Stat(filePath); err == nil {
+		// If the file exists, clear its content
+		err = ioutil.WriteFile(filePath, []byte{}, 0644)
+		if err != nil {
+			fmt.Println("Error clearing file:", err)
+			return
+		}
+	} else if os.IsNotExist(err) {
+		// If the file does not exist, create it
+		_, err = os.Create(filePath)
+		if err != nil {
+			fmt.Println("Error creating file:", err)
+			return
+		}
+	} else {
+		// If there was some other error, print it and return
+		fmt.Println("Error checking file:", err)
+		return
+	}
+
+	// Write the new content to the file
+	err := ioutil.WriteFile(filePath, []byte(fileContent), 0644)
+	if err != nil {
+		fmt.Println("Error writing file:", err)
+		return
+	}
+
+	fmt.Println("File written successfully")
+}
+
+func load_more(link string) *goquery.Document {
+	req, _ := http.NewRequest("GET", link, nil)
+	fmt.Println(link)
+	resp, _ := client.Do(req)
+	doc, _ := goquery.NewDocumentFromReader(resp.Body)
+	return doc
+}
+
+func GetMessages(length int, doc *goquery.Document, number string, channel string) *goquery.Document {
+	x := load_more(channel + "?before=" + number)
+
+	html2, _ := x.Html()
+	reader2 := strings.NewReader(html2)
+	doc2, _ := goquery.NewDocumentFromReader(reader2)
+
+	// _, exist := doc.Find(".js-messages_more").Attr("href")
+	doc.Find("body").AppendSelection(doc2.Find("body").Children())
+
+	newDoc := goquery.NewDocumentFromNode(doc.Selection.Nodes[0])
+	// fmt.Println(newDoc.Find(".js-messages_more").Attr("href"))
+	messages := newDoc.Find(".js-widget_message_wrap").Length()
+
+	fmt.Println(messages)
+	if messages > length {
+		return newDoc
+	} else {
+		num, _ := strconv.Atoi(number)
+		n := num - 21
+		if n > 0 {
+			ns := strconv.Itoa(n)
+			GetMessages(length, newDoc, ns, channel)
+		} else {
+			return newDoc
+		}
+	}
+
+	return newDoc
+}
+
+func reverse(lines []string) []string {
+	for i := 0; i < len(lines)/2; i++ {
+		j := len(lines) - i - 1
+		lines[i], lines[j] = lines[j], lines[i]
+	}
+	return lines
+}
+
+func RemoveDuplicate(config string) string {
+	lines := strings.Split(config, "\n")
+
+	// Use a map to keep track of unique lines
+	uniqueLines := make(map[string]bool)
+
+	// Loop over lines and add unique lines to map
+	for _, line := range lines {
+		if len(line) > 0 {
+			uniqueLines[line] = true
+		}
+	}
+
+	// Join unique lines into a string
+	uniqueString := strings.Join(getKeys(uniqueLines), "\n")
+
+	return uniqueString
+}
+
+func getKeys(m map[string]bool) []string {
+	keys := make([]string, len(m))
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i++
+	}
+	return keys
+}
---
+++ main.py
@@ -0,0 +1,132 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import shutil
+from datetime import datetime
+import urllib.parse
+
+
+def get_v2ray_links(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        divs = soup.find_all('div', class_='tgme_widget_message_text')
+        divs2 = soup.find_all('div', class_='tgme_widget_message_text js-message_text before_footer')
+        spans = soup.find_all('span', class_='tgme_widget_message_text')
+        codes = soup.find_all('code')
+        span = soup.find_all('span')
+        main = soup.find_all('div')
+        
+        all_tags = divs + spans + codes + divs2 + span + main
+
+        v2ray_configs = []
+        for tag in all_tags:
+            text = tag.get_text()
+            if text.startswith('vless://') or text.startswith('ss://') or text.startswith('trojan://') or text.startswith('tuic://'):
+                v2ray_configs.append(text)
+
+        return v2ray_configs
+    else:
+        print(f"Failed to fetch URL (Status Code: {response.status_code})")
+        return None
+
+def get_region_from_ip(ip):
+    api_endpoints = [
+        f'https://ipapi.co/{ip}/json/',
+        f'https://ipwhois.app/json/{ip}',
+        f'http://www.geoplugin.net/json.gp?ip={ip}',
+        f'https://api.ipbase.com/v1/json/{ip}'
+    ]
+
+    for endpoint in api_endpoints:
+        try:
+            response = requests.get(endpoint)
+            if response.status_code == 200:
+                data = response.json()
+                if 'country' in data:
+                    return data['country']
+        except Exception as e:
+            print(f"Error retrieving region from {endpoint}: {e}")
+    return None
+
+def save_configs_by_region(configs):
+    config_folder = "sub"
+    if os.path.exists(config_folder):
+        for folder in os.listdir(config_folder):
+            folder_path = os.path.join(config_folder, folder)
+            if os.path.isdir(folder_path):
+                shutil.rmtree(folder_path)
+
+    if not os.path.exists(config_folder):
+        os.makedirs(config_folder)
+
+    for config in configs:
+        ip = config.split('//')[1].split('/')[0]
+        region = get_region_from_ip(ip)
+        if region:
+            region_folder = os.path.join(config_folder, region)
+            if not os.path.exists(region_folder):
+                os.makedirs(region_folder)
+
+            with open(os.path.join(region_folder, 'config.txt'), 'a', encoding='utf-8') as file:
+                file.write(config + '\n')
+
+
+def create_sub_section():
+    readme_path = "README.md"
+    sub_folder = "sub"
+    found_sub_section = False
+
+    if os.path.exists(readme_path):
+        with open(readme_path, 'r', encoding='utf-8') as readme_file:
+            content = readme_file.read()
+
+            if '## Sub' in content:
+                found_sub_section = True
+
+    new_content = ""
+    new_content += "## Sub\n"
+    new_content += "| Sub |\n"
+    new_content += "|-----|\n"
+
+    for root, dirs, files in os.walk(sub_folder):
+        for directory in dirs:
+            config_path = os.path.join(root, directory, 'config.txt')
+            if os.path.exists(config_path):
+                url = f"https://raw.githubusercontent.com/small1144/Vxray-country/main/sub/{urllib.parse.quote(directory)}/config.txt"
+                new_content += f"| [{directory}]({url}) |\n"
+
+    with open(readme_path, 'w', encoding='utf-8') as readme_file:
+        if found_sub_section:
+            readme_file.write(content.replace(content[content.find('## Sub'):content.find('\n\n', content.find('## Sub'))], new_content))
+        else:
+            readme_file.write(content + new_content)
+
+if __name__ == "__main__":
+    telegram_urls = [
+        "https://t.me/s/v2line",
+        "https://t.me/s/forwardv2ray",
+        "https://t.me/s/VmessProtocol"
+        "https://t.me/s/V2rayProtocol"
+        "https://t.me/s/oneclickvpnkeys"
+        "https://t.me/s/ARv2ray"
+        "https://t.me/s/v2rayng_vpnrog"
+        "https://t.me/s/Watashi_VPN"
+        "https://t.me/s/V2RAYNG_IP"
+        "https://t.me/s/VlessConfig"
+        "https://t.me/s/outlineOpenKey"
+    ]
+
+    all_v2ray_configs = []
+    for url in telegram_urls:
+        v2ray_configs = get_v2ray_links(url)
+        if v2ray_configs:
+            all_v2ray_configs.extend(v2ray_configs)
+
+    if all_v2ray_configs:
+        save_configs_by_region(all_v2ray_configs)
+        create_sub_section()
+        print("Configs saved successfully.")
+    else:
+        print("No V2Ray configs found.")
